7967c11a490f24224eda3195ce01657e46405c51
removed-user <caseyriley94@gmail.com>
2026-07-20 11:40:11 -0400

    Add substring check in starts_with_substring.bash

---
+++ pkgbuild_reader/starts_with_substring.bash
@@ -0,0 +1,8 @@
+if [[ $var == sub_string* ]]; then
+    printf '%s\n' "var starts with sub_string."
+fi
+
+# Inverse (var does not start with sub_string).
+if [[ $var != sub_string* ]]; then
+    printf '%s\n' "var does not start with sub_string."
+fi
